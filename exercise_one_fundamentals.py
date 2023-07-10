56d6a13b9f4f27d00596468c0bf29ed43bce2402
JAAR <joe.aaron.acosta@outlook.com>
2023-07-09 23:46:32 -0400

    Version 0

---
+++ exercise_one_fundamentals.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/09/2023
+>>> Parcticing Fundamentals Program 1
+>>> Version 0
+'''
+
+'''
+>>> Greets the user based on the name they provide then asks the user if they need help with basic mathematics followed by the calculation of both area and perimeter for basic geometric shapes. Afterwards will ask the user for the current temperature to convert it between Fahrenheit or Celsius based on the user input.
+'''
+
+def main() :
+    print('Hello World!')
+
+if __name__ == '__main__' :
+    main()
